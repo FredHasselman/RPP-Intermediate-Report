13c161361210f9507679072a899647281be8aeaf
FredHasselman <me@fredhasselman.com>
2014-05-22 12:59:19 +0200

    naming convention

--- RPP_CastData.R
+++ RPP_CastData.R
@@ -22,6 +22,7 @@ source_https <- function(url, ...) {
 source_https("https://raw.github.com/FredHasselman/scicuRe/master/scicuRe_source.R")
 # The `source_https()` function was found [here](http://tonybreyal.wordpress.com/2011/11/24/source_https-sourcing-an-r-script-from-github/)
 
+# Other functions
 
 # Search patterns 
 real_pat <- paste("(\\d*(","(\\,|\\.)?","\\d+){0,2})",sep="[[:blank:]]?")
--- RPP_Graphs.R
+++ RPP_Graphs.R
@@ -3,6 +3,8 @@ require(MBESS)
 require(plyr)
 require(ggplot2)
 require(RCurl)
+require(metafor)
+require(xlsx)
 
 # SOURCE GITHUB FUNCTIONS -------------------------------------------------
 
@@ -21,17 +23,36 @@ source_https <- function(url, ...) {
 source_https("https://raw.github.com/FredHasselman/scicuRe/master/scicuRe_source.R")
 # The `source_https()` function was found [here](http://tonybreyal.wordpress.com/2011/11/24/source_https-sourcing-an-r-script-from-github/)
 
-## Data were cleaned if test info was missing for anori-rep pair
-# RPPclean <- subset(RPPdata,stat.ori.type!="unknown") 
-# RPPclean <- subset(RPPclean,stat.rep.type!="unknown") 
-
 
 # LOAD DATA FROM GITHUB ---------------------------------------------------
 
-# Complete data
-urltxt   <- getURL("https://raw.githubusercontent.com/FredHasselman/RPP/master/RPPdata.dat")
+## Latest RPP Spreadsheet was converted to tab delimeted file and uploaded to GitHub
+
+# FINdata <- read.xlsx2("RPP master data 18-05-2106.xlsx",sheetName="Statisitics",startRow=2,endRow=32,stringsAsFactors=FALSE)
+# write.table(FINdata,file="RPPdata_master.dat",sep="\t",row.names=T,fileEncoding="UTF-8",quote=F)
+
+urltxt    <- getURL("https://raw.githubusercontent.com/FredHasselman/RPP/master/RPPdata_master.dat")
+RPPmaster <- read.delim(textConnection(urltxt),stringsAsFactors=F)
+
+
+## Complete data after running RPPmasterdata.R
+
+urltxt   <- getURL("https://raw.githubusercontent.com/FredHasselman/RPP/master/RPPdata_cast.dat")
 RPPdata  <- read.delim(textConnection(urltxt),stringsAsFactors=F)
 closeAllConnections()
+
+## Complete pairs of ori-rep for plotting comparisons
+##
+## Data were cleaned if test info was missing for an "ori-rep" pair
+# RPPclean <- subset(RPPdata,stat.ori.type!="unknown") 
+# RPPclean <- subset(RPPclean,stat.rep.type!="unknown") 
+
+urltxt   <- getURL("https://raw.githubusercontent.com/FredHasselman/RPP/master/RPP_cast_pairwise.dat")
+RPPclean <- read.delim(textConnection(urltxt),stringsAsFactors=F)
+closeAllConnections()
+
+
+# LOTS OF PLOTS -----------------------------------------------------------
 
 x1 <- subset(RPPdata,as.numeric(rep.sig)==2)
 x0 <- subset(RPPdata,as.numeric(rep.sig)==1)
@@ -39,8 +60,8 @@ x0 <- subset(RPPdata,as.numeric(rep.sig)==1)
 pdf("RPP_Figures_30studies.pdf",paper="a4r",width=0,height=0)
 
 par(mfrow=c(1,2),pty="s")
-plot(RPPdata$stat.ori.p, RPPdata$stat.ori.p.recalc, xlim=c(0,0.06), ylim=c(0,0.06), xlab="Reported p-value [original study]",ylab="Re-calculated p-value [original study]")
-plot(RPPdata$stat.rep.p,RPPdata$stat.rep.p.recalc,xlab="Reported p-value [replication study]",ylab="Re-calculated p-value [replication study]")
+plot(RPPdata$stat.ori.p, RPPdata$stat.ori.p.recalc, xlim=c(0,0.06), ylim=c(0,0.06), xlab="Reported p-value [original study]",ylab="Re-calculated p-value [original study]",main="Recalculating p-values")
+plot(RPPdata$stat.rep.p,RPPdata$stat.rep.p.recalc,xlab="Reported p-value [replication study]",ylab="Re-calculated p-value [replication study]", main="Recalculating p-values")
 par(mfrow=c(1,1))
 
 ggplot(RPPdata) + geom_point(aes(x=stat.ori.p.recalc, y=stat.rep.p.recalc),size=5) +
@@ -77,5 +98,17 @@ ggplot(RPPdata) + geom_point(aes(x=ES.ori.r, y=ES.rep.r,shape=rep.sig),size=5)  
   labs(title= paste("Original vs. Replication Effect Size (r)\nCorrelation Accept H0 = ",round(cor(x0$ES.rep.r,x0$ES.ori.r,use="complete.obs"), digits=4),"\nCorrelation Reject H0 = ",round(cor(x1$ES.rep.r,x1$ES.ori.r,use="complete.obs"), digits=4)), fill="Inference (Replication)",shape="Inference (Replication)") + 
   xlab("Effect size (r) original") + ylab("Effect size (r) replication") + 
   theme_bw(base_size = 16, base_family = "") + coord_fixed()
+
+ggplot(RPPdata) + geom_point(aes(x=ES.ori.r, y=ES.rep.r,shape=rep.sig),size=5)  +
+  scale_y_continuous(limits=c(0,1),breaks=c(0,.1,.3,.5,1)) + scale_x_continuous(limits=c(0,1),breaks=c(0,.1,.3,.5,1)) +
+  geom_segment(x=.1,xend=.1,y=.1,yend=1.5,colour="red",linetype=2) + geom_segment(x=.1,xend=1.5,y=.1,yend=.1,colour="red",linetype=2) +
+  geom_segment(x=.3,xend=.3,y=.3,yend=1.5,colour="orange",linetype=2) + geom_segment(x=.3,xend=1.5,y=.3,yend=.3,colour="orange",linetype=2) +
+  geom_segment(x=.5,xend=.5,y=.5,yend=1.5,colour="green",linetype=2) + geom_segment(x=.5,xend=1.5,y=.5,yend=.5,colour="green",linetype=2) +
+  geom_smooth(aes(y=ES.rep.r,x=ES.ori.r,group=rep.sig,fill=rep.sig),method="glm",fullrange=FALSE,alpha=.2) + 
+  labs(title= paste("Original vs. Replication Effect Size (r)\nCorrelation Accept H0 = ",round(cor(x0$ES.rep.r,x0$ES.ori.r,use="complete.obs"), digits=4),"\nCorrelation Reject H0 = ",round(cor(x1$ES.rep.r,x1$ES.ori.r,use="complete.obs"), digits=4)), fill="Inference (Replication)",shape="Inference (Replication)") + 
+  xlab("Effect size (r) original") + ylab("Effect size (r) replication") + 
+  theme_bw(base_size = 16, base_family = "") + coord_fixed()
+
+forest(x=)
 
 dev.off()
